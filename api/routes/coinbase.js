f8ff4750fd14b07e945016772d3b8e754f768d9a
Deokjae <deokjj@gmail.com>
2019-05-25 14:00:44 -0400

    use coinbase API instead of HTTPS request with public address

--- api/routes/coinbase.js
+++ api/routes/coinbase.js
@@ -1,31 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+require('dotenv').config();
 
+const Client = require('coinbase').Client;
+const client = new Client({'apiKey': process.env.COINBASE_API_KEY, 
+                         'apiSecret': process.env.COINBASE_API_SECRET});
 
 /* get currency rates  */
-// request body must look like 
-// {
-// 	"currency" : "BTC",
-// 	"ratesFor" : ["USD","CAD"]
-// }
+// request with Coinbase API instead of directly requesting via HTTP
+// when app is deployed, direct HTTP requests are denied by Coinbase.
 router.post('/currency',(req,res,next) => {
   //Base currency
   const {currency, ratesFor} = req.body;
   
-  //Do a HTTP request to coinbase API to get currencies
-  request(`https://api.coinbase.com/v2/exchange-rates${(currency ? '?currency=BTC' : '')}`, { json: true }, 
-  (err, coinbaseRes, body) => {
-    if (err) { res.status(400).json({error:err}); }
+  client.getExchangeRates({'currency': 'BTC'},(err, coinbaseRes) => {
+    if(err) { res.status(400).json({error:err}); }
     
-    //Get only the rates requested
+    //Get only the rates Requested
     const result = {};
-    if(body && body.hasOwnProperty('data') && body['data'].hasOwnProperty('rates')){
-      const rates = body['data']['rates'];
+    if(coinbaseRes && coinbaseRes.hasOwnProperty('data') && coinbaseRes['data'].hasOwnProperty('rates')){
+      const rates = coinbaseRes['data']['rates'];
       ratesFor.forEach((key) => {
         result[key] = rates[key];
       });
-      
+
       //Response with the rates
       res.status(201).json(result);
     }
@@ -36,8 +35,43 @@ router.post('/currency',(req,res,next) => {
       });
     }
   });
-  
 });
+
+/* get currency rates  */
+// request body must look like 
+// {
+// 	"currency" : "BTC",
+// 	"ratesFor" : ["USD","CAD"]
+// }
+// router.post('/currency',(req,res,next) => {
+//   //Base currency
+//   const {currency, ratesFor} = req.body;
+// 
+//   //Do a HTTP request to coinbase API to get currencies
+//   request(`https://api.coinbase.com/v2/exchange-rates${(currency ? '?currency=BTC' : '')}`, { json: true }, 
+//   (err, coinbaseRes, body) => {
+//     if (err) { res.status(400).json({error:err}); }
+// 
+//     //Get only the rates requested
+//     const result = {};
+//     if(body && body.hasOwnProperty('data') && body['data'].hasOwnProperty('rates')){
+//       const rates = body['data']['rates'];
+//       ratesFor.forEach((key) => {
+//         result[key] = rates[key];
+//       });
+// 
+//       //Response with the rates
+//       res.status(201).json(result);
+//     }
+//     else{
+//       console.log("body was not returned properly");
+//       res.status(503).json({
+//         body : body
+//       });
+//     }
+//   });
+// 
+// });
 
 router.get('/price/:conversion',(req,res,next) => {
   console.log(req.params.conversion);
